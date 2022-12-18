4a646cb72d408e5d01efd7c1a775093daf275381
DJ Kehoe <donald.j.kehoe@gmail.com>
2022-12-18 17:05:55 -0500

    fixed a windows compatability bug

--- include/simple_json_types.h
+++ include/simple_json_types.h
@@ -4,7 +4,6 @@
 
 #if defined(WIN32) || defined(_WIN32) || defined(__CYGWIN__) || defined(__MINGW32__)
 
-#if defined(_MSC_VER) && (_MSC_VER < 1600)
 typedef signed __int8 int8_t;
 typedef unsigned __int8 uint8_t;
 typedef signed __int16 int16_t;
@@ -13,7 +12,6 @@ typedef signed __int32 int32_t;
 typedef unsigned __int32 uint32_t;
 typedef signed __int64 int64_t;
 typedef unsigned __int64 uint64_t;
-#endif
 
 #else
 
